4a82f42bbfd5fd6ef47b14e7c2a45f15817c96f6
Earl McBride <Earl_Mcbride@unc.edu>
2024-01-18 23:38:59 -0500

    “Finished Exercise 00!”

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,4 @@
+""""My first program for COMP 110"""
+_author_= "730395850"
+
+print("Hello, wonderful world called earth.")
---
+++ lessons/Basics.py
@@ -0,0 +1,4 @@
+"""Demonstrate the basics of a python program"""
+
+print(type(True))
+first_name = str = input("What is your name")
